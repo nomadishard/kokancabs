1c8cb03597a86d50a01c82d2a5c85655170ccf25
nomadisop <>
2025-03-27 14:41:24 +0400

    upd

--- driverdetails/index.php
+++ driverdetails/index.php
@@ -29,7 +29,7 @@ $id = $_POST['driver_id'];
     <header>
         <nav class="navbar navbar-expand-lg navbar-light bg-white" style="border-bottom: solid;">
             <div class="container-fluid">
-                <a class="navbar-brand" href="../../">KOKANCABS</a>
+                <a class="navbar-brand" href="../">KOKANCABS</a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
